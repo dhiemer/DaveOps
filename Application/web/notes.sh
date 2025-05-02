ce040fffdb11ea88687bc5520f2dff6ebc605f4d
David Hiemer <dhiemer@vertafore.com>
2025-05-02 14:37:35 -0600

    refactor

--- Application/web/notes.sh
+++
@@ -1,6 +0,0 @@
-cd web
-docker build -t localhost:5000/earthquake-web:latest .
-docker push localhost:5000/earthquake-web:latest
-
-cd ..
-helm upgrade --install earthquake-monitor ./charts/earthquake-monitor
--- Terraform/server_kube.tf
+++ Terraform/server_kube.tf
@@ -79,17 +79,17 @@ resource "aws_instance" "k3s_server" {
 
 }
 
-resource "aws_alb_target_group_attachment" "tg_kube_tgattachment" {
-  target_group_arn = aws_lb_target_group.web.arn
-  target_id        = aws_instance.k3s_server.id
-
-}
-
-
-resource "aws_lb_target_group_attachment" "landing" {
-  target_group_arn = aws_lb_target_group.landing.arn
-  target_id        = aws_instance.k3s_server.id
-  port             = 30081
-}
-
+# resource "aws_alb_target_group_attachment" "tg_kube_tgattachment" {
+#   target_group_arn = aws_lb_target_group.web.arn
+#   target_id        = aws_instance.k3s_server.id
+# 
+# }
+# 
+# 
+# resource "aws_lb_target_group_attachment" "landing" {
+#   target_group_arn = aws_lb_target_group.landing.arn
+#   target_id        = aws_instance.k3s_server.id
+#   port             = 30081
+# }
+# 
 
