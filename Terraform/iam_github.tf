d02d8ab2ff5ae969c29485d62a8516595a527c21
David Hiemer <dhiemer@vertafore.com>
2025-05-04 18:15:10 -0600

    update actions

--- Terraform/iam_github.tf
+++ Terraform/iam_github.tf
@@ -1,47 +1,76 @@
+###############################################################################
+# GitHub OIDC provider  (must be a *resource* because it does not exist yet)
+###############################################################################
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  # If you want to future‑proof, keep both IDs.
+  client_id_list = [
+    "sts.amazonaws.com",
+    "https://github.com/dhiemer"
+  ]
+
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+
+  tags = {
+    env = "DaveOps"
+    iac = "true"
+  }
+}
+
+###############################################################################
+# One role that the workflows assume
+###############################################################################
 data "aws_iam_policy_document" "github_oidc_assume_role" {
   statement {
-    effect = "Allow"
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
 
     principals {
       type        = "Federated"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"]
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
     }
 
-    actions = ["sts:AssumeRoleWithWebIdentity"]
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = [
+        "repo:dhiemer/earthquake-monitor:ref:refs/heads/main",
+        "repo:dhiemer/earthquake-monitor:ref:refs/heads/aws"
+      ]
+    }
 
     condition {
       test     = "StringEquals"
-      variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:dhiemer/earthquake-monitor:ref:refs/heads/main","repo:dhiemer/earthquake-monitor:ref:refs/heads/aws"]
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = [
+        "sts.amazonaws.com",
+        "https://github.com/dhiemer"
+      ]
     }
   }
 }
 
-
-
-resource "aws_iam_role" "github_actions_ecr_push" {
-  name               = "github-actions-ecr-push-role"
+resource "aws_iam_role" "github_actions_ecr" {
+  name               = "github-actions-ecr"
   assume_role_policy = data.aws_iam_policy_document.github_oidc_assume_role.json
-
   tags = {
-    Name = "github-actions-ecr-push-role"
+    Name = "github-actions-ecr"
     env  = "DaveOps"
     iac  = "true"
   }
 }
 
-
-
-resource "aws_iam_role_policy" "github_actions_ecr_push_policy" {
-  name = "github-actions-ecr-push-policy"
-  role = aws_iam_role.github_actions_ecr_push.id
+resource "aws_iam_role_policy" "github_actions_ecr_inline" {
+  name = "github-actions-ecr-inline"
+  role = aws_iam_role.github_actions_ecr.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect   = "Allow"
-        Action   = [
+        Effect = "Allow"
+        Action = [
           "ecr:GetAuthorizationToken",
           "ecr:BatchCheckLayerAvailability",
           "ecr:PutImage",
@@ -56,22 +85,3 @@ resource "aws_iam_role_policy" "github_actions_ecr_push_policy" {
     ]
   })
 }
-
-
-# Create the GitHub OIDC provider and IAM role
-module "github-oidc" {
-  source  = "terraform-module/github-oidc-provider/aws"
-  version = "~> 1"
-
-  create_oidc_provider = true
-  create_oidc_role     = true
-  repositories = ["dhiemer/earthquake-monitor"]
-  
-  # Attach AWS managed policies or your custom policies
-  oidc_role_attach_policies = [
-    aws_iam_policy.kube_inline.arn
-    # other policies needed for workflow
-  ]
-}
-
-# arn": "arn:aws:iam::032021926264:role/github-oidc-provider-aws
